09f24a4f32b973cbb47b10a95555419872a49c08
wmylxmj <1397322329@qq.com>
2026-06-03 20:22:46 +0800

    测试volk子模块

--- Runtime/DollsEngine.cpp
+++ Runtime/DollsEngine.cpp
@@ -3,11 +3,20 @@
 
 #include "DollsEngine.h"
 #include "Engine/Engine.h"
+#include "volk.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	VkResult result = volkInitialize();
+
+	if (result != VK_SUCCESS) {
+		printf("volk 初始化失败！请检查系统是否安装了 Vulkan 驱动。\n");
+		return -1;
+	}
+
+	printf("volk 已成功加载 Vulkan 驱动！\n");
+	cout << "Hello CMake!" << endl;
 	return 0;
 }
