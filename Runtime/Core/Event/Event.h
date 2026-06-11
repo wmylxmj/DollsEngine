874eadd33e18a00ac9b268337fd8679243c0e9ae
wmylxmj <1397322329@qq.com>
2026-06-11 21:37:53 +0800

    更新事件

--- Runtime/Core/Event/Event.h
+++ Runtime/Core/Event/Event.h
@@ -3,11 +3,12 @@
 template<typename T>
 constexpr size_t GetEventTypeId()
 {
-	static char typeIdPlaceHolder = 0;
-	return reinterpret_cast<size_t>(&typeIdPlaceHolder);
+	static char typeIdPlaceholder = 0;
+	return reinterpret_cast<size_t>(&typeIdPlaceholder);
 }
 
 class Event
 {
 public:
+	virtual size_t GetEventTypeId() const = 0;
 };
