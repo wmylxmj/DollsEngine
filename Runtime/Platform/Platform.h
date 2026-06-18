7d08ef0ebaa1bcd534f444d779cf175746043f1e
wmylxmj <1397322329@qq.com>
2026-06-18 21:43:04 +0800

    更新平台抽象

--- Runtime/Platform/Platform.h
+++ Runtime/Platform/Platform.h
@@ -2,8 +2,10 @@
 
 #ifdef DOLLS_PLATFORM_WINDOWS
 #include "Windows/WindowsWindow.h"
+#include "Windows/WindowsPlatform.h"
 namespace DollsEngine
 {
 	using NativeWindow = WindowsWindow;
+	using NativePlatform = WindowsPlatform;
 }
 #endif
