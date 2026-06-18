536bb6d7640e614b50ea41ec78ea6fbb82238bf6
wmylxmj <1397322329@qq.com>
2026-06-18 21:39:40 +0800

    更新平台抽象

---
+++ Runtime/Platform/Windows/WindowsPlatform.cpp
@@ -0,0 +1,14 @@
+#include "WindowsPlatform.h"
+
+namespace DollsEngine
+{
+	void WindowsPlatform::PumpMessages()
+	{
+		MSG msg;
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+}
