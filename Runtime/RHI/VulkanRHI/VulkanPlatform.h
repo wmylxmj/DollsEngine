fd7642f5280e580257e2d1e4bdae2ee3783e9356
wmylxmj <1397322329@qq.com>
2026-07-02 22:05:32 +0800

    更新

--- Runtime/RHI/VulkanRHI/VulkanPlatform.h
+++ Runtime/RHI/VulkanRHI/VulkanPlatform.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#ifdef DOLLS_PLATFORM_WINDOWS
+#include "Windows/VulkanWindowsPlatform.h"
+#endif
+
+namespace DollsEngine
+{
+	inline VkResult VulkanPlatformCreateSurface(VkInstance instance, HWND windowHandle, VkSurfaceKHR* outSurface)
+	{
+#ifdef DOLLS_PLATFORM_WINDOWS
+		return VulkanWindowsPlatformCreateSurface(instance, windowHandle, outSurface);
+#endif
+	}
+}
