b227b6f5c8683e19d9750c919aa174258de7f6ea
wmylxmj <1397322329@qq.com>
2026-07-08 20:23:05 +0800

    更新项目

--- Runtime/Application/Platform/Windows/WindowsPlatformService.cpp
+++ Runtime/Application/Platform/Windows/WindowsPlatformService.cpp
@@ -8,5 +8,41 @@
 #include "../../WindowEvents.h"
 
 namespace DollsEngine {
+    bool WindowsPlatformService::Initialize() {
+        WNDCLASSEX wc = {};
+        wc.cbSize = sizeof(WNDCLASSEX);
+        wc.style = CS_DBLCLKS;
+        wc.lpfnWndProc = WndProc;
+        wc.cbClsExtra = 0;
+        wc.cbWndExtra = 0;
+        wc.hInstance = GetModuleHandle(NULL);
+        wc.hIcon = nullptr;
+        wc.hCursor = nullptr;
+        wc.hbrBackground = nullptr;
+        wc.lpszMenuName = nullptr;
+        wc.lpszClassName = TEXT("DOLLS");
+        wc.hIconSm = nullptr;
+
+        if (!RegisterClassEx(&wc))
+        {
+            MessageBox(NULL, TEXT("Window Registration Failed!"), TEXT("Error!"), MB_ICONEXCLAMATION | MB_OK);
+            return false;
+        }
+
+        return true;
+    }
+
+    void WindowsPlatformService::PumpMessages() {
+
+        MSG msg;
+        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+    }
+
 
 }
+
