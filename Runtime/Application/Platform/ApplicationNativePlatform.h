30bb73ddfec897f941fb79f046428ec76c0cf1c8
wmylxmj <1397322329@qq.com>
2026-07-08 21:35:01 +0800

    更新项目

--- Runtime/Application/Platform/ApplicationNativePlatform.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef DOLLS_PLATFORM_WINDOWS
-#include "Windows/ApplicationWindowsPlatform.h"
-namespace DollsEngine
-{
-	using ApplicationNativePlatform = ApplicationWindowsPlatform;
-}
-#endif
