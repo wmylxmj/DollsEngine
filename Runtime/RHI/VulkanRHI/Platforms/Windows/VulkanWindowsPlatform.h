7898135b5818667351f18eaf991f8e0d01d650b4
wmylxmj <1397322329@qq.com>
2026-07-18 19:56:50 +0800

    更新Vulkan扩展

--- Runtime/RHI/VulkanRHI/Platforms/Windows/VulkanWindowsPlatform.h
+++ Runtime/RHI/VulkanRHI/Platforms/Windows/VulkanWindowsPlatform.h
@@ -7,5 +7,6 @@ namespace DollsEngine
 {
 	class VulkanWindowsPlatform : public VulkanPlatform {
 		virtual VkResult CreateSurface(VkInstance instance, void* windowHandle, VkSurfaceKHR* outSurface) override;
+		virtual void CollectInstanceExtensions(VulkanInstanceExtensionsCollector &collector) override;
 	};
 }
--- Runtime/RHI/VulkanRHI/VulkanPlatform.h
+++ Runtime/RHI/VulkanRHI/VulkanPlatform.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "VulkanAPI.h"
+#include "VulkanExtension.h"
+
 
 namespace DollsEngine
 {
 	class VulkanPlatform {
 		virtual VkResult CreateSurface(VkInstance instance, void* windowHandle, VkSurfaceKHR* outSurface) = 0;
-
+		virtual void CollectInstanceExtensions(VulkanInstanceExtensionsCollector& collector) = 0;
 	};
 }
