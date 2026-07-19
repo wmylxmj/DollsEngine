259dd262db624a23ab8502c93454544c64fcba8e
wmylxmj <1397322329@qq.com>
2026-07-19 15:20:03 +0800

    更新Vulkan层

--- Runtime/RHI/VulkanRHI/VulkanLayer.h
+++ Runtime/RHI/VulkanRHI/VulkanLayer.h
@@ -7,6 +7,7 @@ namespace DollsEngine
     class VulkanLayer {
     public:
         explicit VulkanLayer(const char* layerName) : m_layerName(layerName) {}
+        const char* GetLayerName() const { return m_layerName; }
 
     protected:
         const char* m_layerName;
