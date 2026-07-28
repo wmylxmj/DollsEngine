37000820588f6416cc1268cf808e2047f8b6e774
wmylxmj <1397322329@qq.com>
2026-07-28 20:46:23 +0800

    更新VulkanRHI

--- Runtime/RHI/VulkanRHI/VulkanRHI.cpp
+++ Runtime/RHI/VulkanRHI/VulkanRHI.cpp
@@ -94,6 +94,7 @@ namespace DollsEngine
 		vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices.data());
 
 		using Performance = std::tuple<uint32_t>;
+		std::multimap<Performance, VkPhysicalDevice> candidates;
 
 		VkPhysicalDevice selectedDevice = VK_NULL_HANDLE;
 
