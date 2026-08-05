50069b1f5f64739ea649c2c2c401ca19d93af161
wmylxmj <1397322329@qq.com>
2026-08-05 22:14:23 +0800

    更新Vulkan实例

--- Runtime/RHI/VulkanRHI/VulkanInstance.h
+++ Runtime/RHI/VulkanRHI/VulkanInstance.h
@@ -1,11 +1,28 @@
 #pragma once
 
 #include "VulkanAPI.h"
+#include <vector>
 
 namespace DollsEngine
 {
-    class VulkanInstance {
+    // 实例层
+    class VulkanInstanceLayer
+    {
+    public:
+        explicit VulkanInstanceLayer(const char* layerName) : m_layerName(layerName) {}
+        const char* GetLayerName() const { return m_layerName; }
+        void SetSupported() { m_isSupported = true; }
+        bool IsSupported() const { return m_isSupported; }
 
+
+    protected:
+        const char* m_layerName;
+        bool m_isSupported = false;
+
+    };
+
+    class VulkanInstance {
+    private:
     };
 }
 
