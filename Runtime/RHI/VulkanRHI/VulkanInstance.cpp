551abb7b627ae309cbd618f956d034bb89df45d4
wmylxmj <1397322329@qq.com>
2026-08-08 14:24:48 +0800

    更新Vulkan实例

--- Runtime/RHI/VulkanRHI/VulkanInstance.cpp
+++ Runtime/RHI/VulkanRHI/VulkanInstance.cpp
@@ -10,7 +10,24 @@ namespace DollsEngine
 {
     void VulkanInstance::FlagLayersSupported()
     {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (auto& preferredLayer : m_preferredLayers) {
+            if (preferredLayer.IsSupported()) {
+                continue;
+            }
+            for (const auto& availableLayer : availableLayers) {
+                if (strcmp(preferredLayer.GetLayerName(), availableLayer.layerName) == 0) {
+                    preferredLayer.SetSupported();
+                    break;
+                }
+            }
+        }
     }
+
 
 }
