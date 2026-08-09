a74b1b054b701d65922dd08340967160c03ba724
wmylxmj <1397322329@qq.com>
2026-08-09 16:52:57 +0800

    更新Vulkan平台

--- Runtime/RHI/VulkanRHI/Platforms/Windows/VulkanWindowsPlatform.h
+++ Runtime/RHI/VulkanRHI/Platforms/Windows/VulkanWindowsPlatform.h
@@ -7,6 +7,7 @@ namespace DollsEngine
 {
 	class VulkanWindowsPlatform : public VulkanPlatform {
 	public:
+		virtual void AddPreferredPlatformSpecificInstanceExtensions(VulkanInstance &instance) override;
 		virtual VkResult CreateSurface(VkInstance instance, void* windowHandle, VkSurfaceKHR* outSurface) override;
 		virtual void CollectInstanceExtensions(VulkanInstanceExtensionsCollector &collector) override;
 	};
