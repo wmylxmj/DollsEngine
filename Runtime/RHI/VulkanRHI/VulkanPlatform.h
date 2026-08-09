f3748c7d4f97c95cd2ab77f9401a76d8d35e9fc4
wmylxmj <1397322329@qq.com>
2026-08-09 17:29:03 +0800

    更新Vulkan平台

--- Runtime/RHI/VulkanRHI/VulkanPlatform.h
+++ Runtime/RHI/VulkanRHI/VulkanPlatform.h
@@ -2,17 +2,14 @@
 
 #include "VulkanAPI.h"
 #include "VulkanInstance.h"
-#include "VulkanExtension.h"
-
 
 namespace DollsEngine
 {
-	class VulkanPlatform {
+	class VulkanPlatform
+	{
 	public:
 		virtual void AddPreferredPlatformSpecificInstanceExtensions(VulkanInstance& instance) = 0;
 
 		virtual VkResult CreateSurface(VkInstance instance, void* windowHandle, VkSurfaceKHR* outSurface) = 0;
-
-		virtual void CollectInstanceExtensions(VulkanInstanceExtensionsCollector& collector) = 0;
 	};
 }
