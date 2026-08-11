25020d1d97d516d924e503e6e53532bca7d9fc60
wmylxmj <1397322329@qq.com>
2026-08-11 22:43:58 +0800

    更新Vulkan物理设备

---
+++ Runtime/RHI/VulkanRHI/VulkanPhysicalDevice.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 13973 on 26-8-11.
+//
+
+#include "VulkanPhysicalDevice.h"
---
+++ Runtime/RHI/VulkanRHI/VulkanPhysicalDevice.h
@@ -0,0 +1,16 @@
+//
+// Created by 13973 on 26-8-11.
+//
+
+#ifndef VULKANPHYSICALDEVICE_H
+#define VULKANPHYSICALDEVICE_H
+
+
+
+class VulkanPhysicalDevice {
+
+};
+
+
+
+#endif //VULKANPHYSICALDEVICE_H
