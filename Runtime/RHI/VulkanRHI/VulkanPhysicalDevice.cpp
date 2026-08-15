cfdd5057ec2bdf4300f641b0c2ef50b94989bedc
wmylxmj <1397322329@qq.com>
2026-08-15 16:23:33 +0800

    更新Vulkan物理设备

--- Runtime/RHI/VulkanRHI/VulkanPhysicalDevice.cpp
+++ Runtime/RHI/VulkanRHI/VulkanPhysicalDevice.cpp
@@ -1,5 +1,10 @@
-//
-// Created by 13973 on 26-8-11.
-//
-
 #include "VulkanPhysicalDevice.h"
+
+namespace DollsEngine
+{
+    void VulkanPhysicalDevice::Initialize(VkPhysicalDevice physicalDevice)
+    {
+
+    }
+
+}
