7e7a45684b5e20ca829c62f6e21504855800c538
wmylxmj <1397322329@qq.com>
2026-08-15 16:00:27 +0800

    更新Vulkan物理设备

--- Runtime/RHI/VulkanRHI/VulkanPhysicalDevice.h
+++ Runtime/RHI/VulkanRHI/VulkanPhysicalDevice.h
@@ -9,7 +9,7 @@ namespace DollsEngine
  		VkPhysicalDevice m_physicalDevice;
 		VkPhysicalDeviceProperties m_physicalDeviceProperties;
 		VkPhysicalDeviceIDPropertiesKHR m_physicalDeviceIDProperties;
-
+    	VkPhysicalDeviceSubgroupProperties m_physicalDeviceSubgroupProperties;
 
     };
 }
