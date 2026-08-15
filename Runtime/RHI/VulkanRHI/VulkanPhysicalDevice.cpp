091e3cec75f78fe99b8ee20cbe1a6153b6d6df9c
wmylxmj <1397322329@qq.com>
2026-08-15 16:26:50 +0800

    更新Vulkan物理设备

--- Runtime/RHI/VulkanRHI/VulkanPhysicalDevice.cpp
+++ Runtime/RHI/VulkanRHI/VulkanPhysicalDevice.cpp
@@ -4,6 +4,15 @@ namespace DollsEngine
 {
     void VulkanPhysicalDevice::Initialize(VkPhysicalDevice physicalDevice)
     {
+        m_physicalDevice = physicalDevice;
+
+        m_physicalDeviceIDProperties = {};
+        m_physicalDeviceIDProperties.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_ID_PROPERTIES_KHR;
+        m_physicalDeviceSubgroupProperties = {};
+        m_physicalDeviceSubgroupProperties.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SUBGROUP_PROPERTIES;
+
+        VkPhysicalDeviceProperties2KHR physicalDeviceProperties2 = {};
+        physicalDeviceProperties2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2_KHR;
 
     }
 
