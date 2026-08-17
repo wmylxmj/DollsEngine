cdbd28afb89229b87009b898b01aa25f08a8b222
wmylxmj <1397322329@qq.com>
2026-08-17 20:41:51 +0800

    更新Vulkan平台

--- Runtime/RHI/VulkanRHI/VulkanDevice.h
+++ Runtime/RHI/VulkanRHI/VulkanDevice.h
@@ -10,6 +10,16 @@ namespace DollsEngine
 	class VulkanDeviceExtension
 	{
 	public:
+        const char* GetExtensionName() const { return m_extensionName; }
+        void SetSupported() { m_isSupported = true; }
+        [[nodiscard]] bool IsSupported() const { return m_isSupported; }
+
+    protected:
+        const char* m_extensionName;
+        bool m_isSupported = false;
+    };
+
+
 	};
 
 	class VulkanDevice
