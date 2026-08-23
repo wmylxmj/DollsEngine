7ceae73ccc8981ec86b53376e557196b914db6b3
wmylxmj <1397322329@qq.com>
2026-08-23 16:38:53 +0800

    更新Vulkan设备

--- Runtime/RHI/VulkanRHI/VulkanDevice.h
+++ Runtime/RHI/VulkanRHI/VulkanDevice.h
@@ -12,9 +12,6 @@ namespace DollsEngine
 	class VulkanDevice
 	{
 	public:
-		void AddPreferredExtension(const char* extensionName) { m_preferredExtensions.emplace_back(false, extensionName); }
-
-
 		bool Create(VulkanPhysicalDevice physicalDevice);
 
 	private:
