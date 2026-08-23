2103d8a45de8a72c2557564f99bd576e410401fe
wmylxmj <1397322329@qq.com>
2026-08-23 16:30:34 +0800

    更新Vulkan实例

--- Runtime/RHI/VulkanRHI/VulkanDevice.h
+++ Runtime/RHI/VulkanRHI/VulkanDevice.h
@@ -9,20 +9,6 @@
 
 namespace DollsEngine
 {
-	// 设备拓展
-	class VulkanDeviceExtension
-	{
-	public:
-		explicit VulkanDeviceExtension(const char* extensionName) : m_extensionName(extensionName) {}
-        const char* GetExtensionName() const { return m_extensionName; }
-        void SetSupported() { m_isSupported = true; }
-        [[nodiscard]] bool IsSupported() const { return m_isSupported; }
-
-    protected:
-        const char* m_extensionName;
-        bool m_isSupported = false;
-    };
-
 	class VulkanDevice
 	{
 	public:
