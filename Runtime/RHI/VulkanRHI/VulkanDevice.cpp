7829ffe7e24ff6d51caa0254521709864f1cef9e
wmylxmj <1397322329@qq.com>
2026-09-01 10:27:10 +0800

    更新设备

--- Runtime/RHI/VulkanRHI/VulkanDevice.cpp
+++ Runtime/RHI/VulkanRHI/VulkanDevice.cpp
@@ -10,7 +10,17 @@ namespace DollsEngine
     }
 
     void VulkanDevice::FlagExtensionsSupported(const char *layerName) {
+        uint32_t extensionCount;
+        vkEnumerateDeviceExtensionProperties(m_physicalDevice.GetHandle(), layerName, &extensionCount, nullptr);
 
+        std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+        vkEnumerateDeviceExtensionProperties(m_physicalDevice.GetHandle(), layerName, &extensionCount, availableExtensions.data());
+
+        for (const char *requiredExtension : m_requiredExtensions) {
+            bool hasExtension = false;
+            for (const VkExtensionProperties &availableExtension : availableExtensions) {
+                if (strcmp(requiredExtension, availableExtension.extensionName) == 0) {
+                    hasExtension = true;
     }
 
 
