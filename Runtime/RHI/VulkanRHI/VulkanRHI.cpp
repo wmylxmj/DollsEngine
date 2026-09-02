acb7456d46f1542b23b1cb62cd6ca92e015fc542
wmylxmj <1397322329@qq.com>
2026-09-02 10:06:28 +0800

    更新设备

--- Runtime/RHI/VulkanRHI/VulkanRHI.cpp
+++ Runtime/RHI/VulkanRHI/VulkanRHI.cpp
@@ -62,7 +62,7 @@ namespace DollsEngine
 		createInfo.pfnUserCallback = DebugMessageCallback;
 		createInfo.pUserData = nullptr; // Optional
 
-		if (vkCreateDebugUtilsMessengerEXT(m_instance.GetInstance(), &createInfo, nullptr, &m_debugMessenger) != VK_SUCCESS) {
+		if (vkCreateDebugUtilsMessengerEXT(m_instance.GetHandle(), &createInfo, nullptr, &m_debugMessenger) != VK_SUCCESS) {
 			return false;
 		}
 
