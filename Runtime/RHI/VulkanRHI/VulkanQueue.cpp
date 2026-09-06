ef66211ad45a94663222c4d024f7799a27f7ee5c
wmylxmj <1397322329@qq.com>
2026-09-06 14:47:07 +0800

    更新Vulkan队列

--- Runtime/RHI/VulkanRHI/VulkanQueue.cpp
+++ Runtime/RHI/VulkanRHI/VulkanQueue.cpp
@@ -4,11 +4,16 @@
 
 #include "VulkanQueue.h"
 
+#include "VulkanDevice.h"
+
 namespace DollsEngine
 {
     void VulkanQueue::Initialize(uint32_t queueFamilyIndex, uint32_t queueIndex)
     {
+        m_queueFamilyIndex = queueFamilyIndex;
+        m_queueIndex = queueIndex;
 
+        vkGetDeviceQueue(m_device.GetHandle(), m_queueFamilyIndex, m_queueIndex, &m_queue);
     }
 
 }
