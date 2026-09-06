58612656068d35041f801e9e84ffd9c0e5dadcc5
wmylxmj <1397322329@qq.com>
2026-09-06 14:35:09 +0800

    更新Vulkan队列

--- Runtime/RHI/VulkanRHI/VulkanQueue.h
+++ Runtime/RHI/VulkanRHI/VulkanQueue.h
@@ -10,6 +10,7 @@ namespace DollsEngine
     public:
         VulkanQueue(VulkanDevice& device);
 
+        void Initialize(uint32_t queueFamilyIndex, uint32_t queueIndex);
         VkQueue GetHandle() const { return m_queue; }
         uint32_t GetQueueFamilyIndex() const { return m_queueFamilyIndex; }
 
