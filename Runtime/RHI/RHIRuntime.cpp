4b6913ae9cb938d23a0e5503f8def54135e4c973
wmylxmj <1397322329@qq.com>
2026-09-19 14:44:54 +0800

    更新RHI

---
+++ Runtime/RHI/RHIRuntime.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 13973 on 26-9-19.
+//
+
+#include "RHIRuntime.h"
---
+++ Runtime/RHI/RHIRuntime.h
@@ -0,0 +1,16 @@
+//
+// Created by 13973 on 26-9-19.
+//
+
+#ifndef RHIRUNTIME_H
+#define RHIRUNTIME_H
+
+
+
+class RHIRuntime {
+
+};
+
+
+
+#endif //RHIRUNTIME_H
